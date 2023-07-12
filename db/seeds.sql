00fc200ea56ab65b40d7e68b74d61e38dd1ea4af
TheWiseSailor <rudasilljay@gmail.com>
2023-07-12 15:01:22 -0400

    stopped due to certain issues irl

---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+--inserting all the departments that the bootcamp sample has on It
+INSERT INTO department(department_id, name)
+VALUES
+    (1, 'Sales'),
+    (2, 'Engineering'),
+    (3, 'Finance'),
+    (4, 'Legal');
+    (5, 'Marketing');
+    (6, 'Human Resources')
+  INSERT INTO role(role_id, title, salary, department_id)
+  VALUES
+  (1, 'Sales Lead', 100000, 1),
+  (2, 'Salesperson', 100000, 1),
+  (3, 'SLead Engineer', 100000, 1),
+  (4, 'Software Engineer', 100000, 1),
+  (5, 'Accountant', 100000, 1),
+  (6, 'Legal Team', 100000, 1),
+  (7, 'Marketing', 100000, 1),
+  ( , , ,)
+  ( , , ,)
+  ( , , ,)
+  ( , , ,)
